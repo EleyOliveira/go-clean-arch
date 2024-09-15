66ddaac3b1cee46d716318006c4256c4de16839e
EleyOliveira <eleyoliveira@bol.com.br>
2024-09-14 21:40:52 -0300

    inserção de pedido pelo GraphQL

--- internal/infra/graphql/graph/schema.resolvers.go
+++ internal/infra/graphql/graph/schema.resolvers.go
@@ -6,14 +6,28 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/EleyOliveira/go-clean-arch/internal/infra/graphql/graph/model"
+	"github.com/EleyOliveira/go-clean-arch/internal/usecase"
 )
 
 // CreateOrder is the resolver for the createOrder field.
 func (r *mutationResolver) CreateOrder(ctx context.Context, input *model.OrderInput) (*model.Order, error) {
-	panic(fmt.Errorf("not implemented: CreateOrder - createOrder"))
+	dto := usecase.OrderInputDTO{
+		ID:    input.ID,
+		Price: float64(input.Price),
+		Tax:   float64(input.Tax),
+	}
+	output, err := r.CreateOrderUseCase.Execute(dto)
+	if err != nil {
+		return nil, err
+	}
+	return &model.Order{
+		ID:         output.ID,
+		Price:      float64(output.Price),
+		Tax:        float64(output.Tax),
+		FinalPrice: float64(output.FinalPrice),
+	}, nil
 }
 
 // Orders is the resolver for the Orders field.
